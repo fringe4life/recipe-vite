5a38c79a3525169b73ffc78888f42164e5a37bdf
Coinnich <>
2025-01-27 15:40:45 +1300

    updated path aliases to include fonts

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,25 @@
 import { defineConfig } from 'vite';
 import path from 'node:path';
-import fs from 'node:fs';
-import {glob} from 'glob';
+import { glob } from 'glob';
 
 // Function to get all HTML files
 const getAllHtmlFiles = (dir: string): string[] => {
   return glob.sync(`${dir}/**/*.html`);
 };
 
-// Get all HTML files from the src directory
-const htmlFiles = getAllHtmlFiles(path.resolve(__dirname, 'src'));
+// Get all HTML files from the root directory
+const htmlFiles = getAllHtmlFiles(path.resolve(__dirname));
 
 // Vite config
 export default defineConfig({
   plugins: [],
   resolve: {
     alias: {
-			"@": path.resolve(__dirname, "src"),
-			"@images": path.resolve(__dirname, "/src/images"),
-			"@styles": path.resolve(__dirname, "/src/styles"),
-		}
+      "@": path.resolve(__dirname, "src"),
+      "@images": path.resolve(__dirname, "src/images"),
+      "@styles": path.resolve(__dirname, "src/styles"),
+      "@fonts": path.resolve(__dirname, "src/fonts"),
+    }
   },
   build: {
     minify: true,
@@ -33,6 +33,6 @@ export default defineConfig({
   },
   esbuild: {
     // Disable console and debugger in production
-      drop: ['console', 'debugger'],
+    drop: ['console', 'debugger'],
   },
 });
